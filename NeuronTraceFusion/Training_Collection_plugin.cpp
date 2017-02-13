f45f5583cba4a0c0791f8b65b07f634a8c29691a
rsuvanto <suvantorw@mx.lakeforest.edu>
2017-02-13 10:37:54 -0600

    Add files via upload

--- NeuronTraceFusion/Training_Collection_plugin.cpp
+++ NeuronTraceFusion/Training_Collection_plugin.cpp
@@ -2,19 +2,9 @@
  * Gather training data for nueron reconstruction
  * 2016-10-25 : by Randall W. Suvanto
  */
- 
-#include "v3d_message.h"
-#include <vector>
-#include <cmath>
-#include <sstream>
-#include <algorithm>
-#include "my_surf_objs.h"
+
 #include "Training_Collection_plugin.h"
-#include "basic_surf_objs.h"
-#include "basic_4dimage.h"
-#include "SVMClassifier.h"
-#include <time.h>
-#include <fstream>
+
 
 
 //void SVMClassifier::train(float*, int*, int, int);
@@ -28,28 +18,7 @@ svm_model *pmodel1;
 svm_model *pmodel2;
 svm_model *pmodel3;
 
-struct ROISegment
-{
-    int x_start;
-    int x_end;
-    int y_start;
-    int y_end;
-    int z_start;
-    int z_end;
-    
-    int originalx1;
-    int originalx2;
-    int originaly1;
-    int originaly2;
-    int originalz1;
-    int originalz2;
-};
 
-bool setUpTrainingData(vector<Image4DSimple*> OneDImageData, vector<Image4DSimple*> TwoDImageData, vector<Image4DSimple*> ThreeDImageData, int averagex1D, int averagex2D, int averagey2D,int xDim, int yDim, int zDim, int numeberOfSamples, int numberOfSamples1D, int numberofSamples2D, vector<ROISegment> ROIs1D, vector<ROISegment> ROIs2D, vector<ROISegment> ROIs3D, vector<Image4DSimple*> Negative1DImageData, vector<Image4DSimple*> Negative2DImageData, vector<Image4DSimple*> Negative3DImageData, int maxNumberOf2DSamples, int maxNumberOf3DSamples, V3DPluginCallback2 &callback, QWidget *parent, unsigned char OneDArrayData[], unsigned char negativeArrayData1D[], unsigned char TwoDArrayData[], unsigned char negativeArrayData2D[], unsigned char ThreeDArrayData[], unsigned char negativeArrayData3D[]);
-
-bool classifySamples(SVMClassifier* OneDClassifier, SVMClassifier* TwoDClassifier, SVMClassifier* ThreeDClassifier, int OneDImageArraySize, int TwoDImageArraySize, int ThreeDImageArraySize, int TwoDXDim, int TwoDYDim, int ThreeDXDim, int ThreeDYDim, int ThreeDZDim, V3DPluginCallback2 &callback,  QWidget *parent);
-
-int prePredictedValue(ROISegment thisSegment);
 
 QStringList Training::menulist() const
 {
