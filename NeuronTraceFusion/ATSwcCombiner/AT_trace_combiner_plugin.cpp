c9db59acaa36fe8329fa744ac254cf7dc38845f0
Jie Zhou <jzhou@niu.edu>
2017-02-24 11:46:32 -0600

    link the meanshifting with the node connection

--- NeuronTraceFusion/ATSwcCombiner/AT_trace_combiner_plugin.cpp
+++ NeuronTraceFusion/ATSwcCombiner/AT_trace_combiner_plugin.cpp
@@ -8,7 +8,11 @@
  *
  * 2017-2-13:  Revised.  Add the raw image so that machine learning is used to validate the decision for connection.
  *
- * Usage ./vaa3d64 -x AT_trace_combiner -f combine_swc -i meanshifted.swc image.tiff -p 20
+ * Usage ./vaa3d64 -x AT_trace_combiner -f combine_swc -i meanshifted.swc -p 20 image.tiff
+ *
+ * 2017-2.24:  Add the call to the mean-shifting code so that the pipeline takes in an ANO file, mean-shift, before connect.
+ *
+ * Usage ./vaa3d64 -x AT_trace_combiner -f combine_swc -i input.ano -p 20 image.tiff
  *
  */
  
@@ -17,13 +21,15 @@
 #include <iostream>
 #include "AT_trace_combiner_plugin.h"
 #include "connect_node.h"
+#include "../Mean_Shift/Mean_Shift.h"
 
 using namespace std;
+vector<NeuronTree> load_linker_file(QString qs_linker);
 Q_EXPORT_PLUGIN2(AT_trace_combiner, ATTraceCombinerPlugin);
 
 struct input_PARA
 {
-    QString inswc_file;
+    QString inano_file;
     QString inimg_file;
     V3DLONG channel = 1;
     int threshold = 0 ;
@@ -67,9 +73,6 @@ void ATTraceCombinerPlugin::domenu(const QString &menu_name, V3DPluginCallback2 
 bool ATTraceCombinerPlugin::dofunc(const QString & func_name, const V3DPluginArgList & input, V3DPluginArgList & output, V3DPluginCallback2 & callback,  QWidget * parent)
 {
 	vector<char*> infiles, inparas, outfiles;
-    //if(input.size() >= 1) infiles = *((vector<char*> *)input.at(0).p);
-    //if(input.size() >= 2) inparas = *((vector<char*> *)input.at(1).p);
-    //if(output.size() >= 1) outfiles = *((vector<char*> *)output.at(0).p);
 
 	if (func_name == tr("combine_swc"))
 	{
@@ -87,62 +90,41 @@ bool ATTraceCombinerPlugin::dofunc(const QString & func_name, const V3DPluginArg
         }
         else
         {
-            PARA.inswc_file = infiles[0];
+            PARA.inano_file = infiles[0];
         }
         int k=0;
         PARA.threshold = (paras.size() >= k+1) ? atoi(paras[k]) : 998;  k++;
         PARA.inimg_file = (paras.size() >= k+1) ? paras[k] : "FileNotExist";  k++; //use parameter to get the 2cond file
 
 
-        /*
-        V3DLONG N,M,P,sc,c;
-        V3DLONG in_sz[4];
-
-        int datatype = 0;
-        if (!simple_loadimage_wrapper(callback,PARA.inimg_file.toStdString().c_str(), data1d, in_sz, datatype))
-        {
-            fprintf (stderr, "Error happens in reading the subject file [%s]. Exit. \n",PARA.inimg_file.toStdString().c_str());
-            return;
-        }
-        if(PARA.channel < 1 || PARA.channel > in_sz[3])
-        {
-            fprintf (stderr, "Invalid channel number. \n");
-            return;
-        }
-
-        N = in_sz[0];
-        M = in_sz[1];
-        P = in_sz[2];
-        sc = in_sz[3];
-         */
         /////////////////////////////////////// CODE START HERE ///////
-        //V3DLONG N = 1084, M = 782, P = 59;  //pass in later
-        //cout << "width: "  << N << endl;
-        //cout << "height: "  << M << endl;
-        //cout << "depth: "  << P << endl;
 
-        QList<NeuronSWC> nodeList;
+        //1. load the linker file to get vector<NeuronTree>
+        vector<NeuronTree> nt_list = load_linker_file(PARA.inano_file);
+        if (nt_list.size() == 0)  return false;
 
-        cout << "read the swc file ..." << endl;
-        nodeList = readSWC_file(PARA.inswc_file).listNeuron;
-
+        //2. load the image
         cout << "load the image ... "  << endl;
         V3DLONG in_sz[4];
         int datatype = 0;
         unsigned char * data1d = 0;
-
-        cout << "starting loading ... " << endl;
+        //need to get from ANO file. Change later.
         if (!simple_loadimage_wrapper(callback,PARA.inimg_file.toStdString().c_str(), data1d, in_sz, datatype))
         {
             fprintf (stderr, "Error happens in reading the subject file [%s]. Exit. \n",PARA.inimg_file.toStdString().c_str());
             return false;
         }
 
+        //3. call meanshifting on each neuron tree, combine the meanshifted ones into one long list
+        QList<NeuronSWC> nodeList= meanShift(nt_list, data1d);
+
+        //4. call connect_node() to get the nodes to get a new swc file
         cout << "calling connect node ...  " << endl;
-       // QHash<V3DLONG, NeuronSWC> nodeMap = connect_node(nodeList, PARA.threshold, N, M, P);
+        //QList<NeuronSWC> nodeList = readSWC_file(PARA.inswc_file).listNeuron;
         QHash<V3DLONG, NeuronSWC> nodeMap = connect_node(nodeList, PARA.threshold, in_sz, data1d);
 
-        save_swc(nodeMap, "output_connect_node.swc", PARA.inswc_file);
+        // save to a new swc file, the input file is for information only
+        save_swc(nodeMap, "output_connect_node.swc", PARA.inano_file);
         cout << "output saved to output_connect_node.swc" << endl;
 
 	}
@@ -157,12 +139,48 @@ bool ATTraceCombinerPlugin::dofunc(const QString & func_name, const V3DPluginArg
 }
 
 void print_help(){
-   cout<<"Example: v3d -x AT_trace_combiner -f combine_swc -i input.swc -p threshold \n"<<endl;
+   cout<<"Example: v3d -x AT_trace_combiner -f combine_swc -i input.ano -p threshold \n"<<endl;
+}
+
+
+vector<NeuronTree> load_linker_file(QString qs_linker)
+{
+        //read in ANO file which contains one image and multiple swc files
+        vector<NeuronTree> nt_list;
+        QStringList nameList;
+        //QString qs_linker;
+        char * dfile_result = NULL;
+        V3DLONG neuronNum = 0;
+
+        if (qs_linker.toUpper().endsWith(".ANO"))
+        {
+            cout<<"Read in a linker (ANO)) file."<<endl;
+            P_ObjectFileType linker_object;
+            if (!loadAnoFile(qs_linker,linker_object))
+            {
+                fprintf(stderr,"Error in reading the linker file.\n");
+                return nt_list;
+            }
+            nameList = linker_object.swc_file_list; //get the swc list from ANO
+            neuronNum += nameList.size();  //number of swc files
+            for (V3DLONG i=0;i<neuronNum;i++)
+            {
+                 NeuronTree tmp = readSWC_file(nameList.at(i)); //read each swc file here
+                 nt_list.push_back(tmp);  //build the internal list for neuron trees
+            }
+            cout << "ANO file successfully read!!\n";
+         }else
+         {
+                 cout << "Expect an ANO file!" << endl;
+         }
+
+        return nt_list;
+
 }
 
 
 /*
-bool load_linker(const V3DPluginArgList & input, V3DPluginArgList & output, V3DPluginCallback2 &callback)
+bool load_linker_file(const V3DPluginArgList & input, V3DPluginArgList & output, V3DPluginCallback2 &callback)
 {
      //get input: the image, and a list of swc files.
      if(input.size()==0 || output.size() != 1) return false;
@@ -215,20 +233,11 @@ bool load_linker(const V3DPluginArgList & input, V3DPluginArgList & output, V3DP
 
     cout << "ANO file successfully read!!\n";
 
-    //verify they are corrected loaded by outputting to one file
+
 }
 
 void print_help(){
    cout<<"Example: v3d -x AT_trace_combiner -f combine_swc -i mylinker.ano -o combined.swc \n"<<endl;
-}
-
-
-void meanshiftingTheSwcNodes(vector<NeuronTree> nt_list)
-{
-    //One each NeuronTree, call code in vaa3d_tools/released_plugins/v3d_plugins/mean_shift_center
-
-    //call mean_shift(swc, ....);
-
 }
 
 */
